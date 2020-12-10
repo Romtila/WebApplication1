130c576aae4e58593da644e1d3a2610c4883854d
Romtila <romtila@yandex.ru>
2020-12-10 14:40:58 +1000

    Add UserView

---
+++ WebApplication1/Models/UserView.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using System;
+
+namespace WebApplication1 // как зарегать generic, зарегать тот Baserepository, 
+{
+    public class UserView
+    {
+        public ObjectId Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime BirthDay { get; set; }
+    }
+}
